05b08999cabc93ab688466b99e82c4a914df9c40
Fael123Programming <rafaelfonseca1020@gmail.com>
2022-03-27 22:44:17 -0300

    connection with MySql database

--- src/main/java/com/rafael/springboot/domain/car/Car.java
+++ src/main/java/com/rafael/springboot/domain/car/Car.java
@@ -2,11 +2,23 @@ package com.rafael.springboot.domain.car;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
+
 public class Car {
     private String brand, model;
     private Color color;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 }
--- src/main/java/com/rafael/springboot/domain/customer/Customer.java
+++ src/main/java/com/rafael/springboot/domain/customer/Customer.java
@@ -2,10 +2,22 @@ package com.rafael.springboot.domain.customer;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data //To provide this class with all auxiliary methods (such as getters, setters, toString, equals etc).
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
+
 public class Customer {
     private String name;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 }
